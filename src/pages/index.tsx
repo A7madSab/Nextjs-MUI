e19c3b566a8776676639b38cd8ace2ca79bc1d34
Ahmad Sabry <ahmadsabry330@gmail.com>
2021-07-04 22:51:15 +0200

    Upgraded Next & React

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Typography from "@material-ui/core/Typography"
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <Typography color="error">Lets Start</Typography>
       <Typography color="textPrimary">Lets Start</Typography>
       <Typography color="textSecondary">Lets Start</Typography>
+      <Image className="rotating" src="/assets/victory.png" alt="victory" width="100px" height="100px" />
     </>
   )
 }
